fa06de00bd12d0e5aaaddf039c3cdfc28d3fcb47
Tom Holden <thomas.holden@gmail.com>
2019-04-03 18:20:44 +0200

    rename deflect

--- compute_deflected_linear_approximation.m
+++ compute_deflected_linear_approximation.m
@@ -1,4 +1,4 @@
-function [ deflect_, M, oo ] = compute_deflected_linear_approximation( M, options, oo, ModeRSS1OrMean2 )
+function [ deflect, M, oo ] = compute_deflected_linear_approximation( M, options, oo, ModeRSS1OrMean2 )
 
 % Copyright: Alexander Meyer-Gohde 2016, Tom Holden 2018 (only slight modifications from TH)
 
@@ -7,7 +7,7 @@ function [ deflect_, M, oo ] = compute_deflected_linear_approximation( M, option
 
 if ModeRSS1OrMean2 > 2
     disp('Incompatible mode')
-    deflect_=[];
+    deflect=[];
     return
 end
 
@@ -32,27 +32,27 @@ if options.order>=2
 end
 
 if options.order>=1
-    deflect_.y=oo.dr.ys(oo.dr.order_var);
-    deflect_.y_x=oo.dr.ghx;
-    deflect_.y_u=oo.dr.ghu;
+    deflect.y=oo.dr.ys(oo.dr.order_var);
+    deflect.y_x=oo.dr.ghx;
+    deflect.y_u=oo.dr.ghu;
 end
 if options.order>=2
     if ModeRSS1OrMean2==2
         x_2=(eye(nspred)-oo.dr.ghx(nstatic+1:nstatic+nspred,:))\(oo.dr.ghuu(nstatic+1:nstatic+nspred,:)*vec(M.Sigma_e)+oo.dr.ghxx(nstatic+1:nstatic+nspred,:)*vec(state_var)+oo.dr.ghs2(nstatic+1:nstatic+nspred,:));
         y_2=[oo.dr.ghx(1:nstatic,:)*x_2+oo.dr.ghuu(1:nstatic,:)*vec(M.Sigma_e)+oo.dr.ghxx(1:nstatic,:)*vec(state_var)+oo.dr.ghs2(1:nstatic,:);x_2;oo.dr.ghx(nstatic+nspred+1:M.endo_nbr,:)*x_2+oo.dr.ghuu(nstatic+nspred+1:M.endo_nbr,:)*vec(M.Sigma_e)+oo.dr.ghxx(nstatic+nspred+1:M.endo_nbr,:)*vec(state_var)+oo.dr.ghs2(nstatic+nspred+1:M.endo_nbr,:)];
-        deflect_.y=deflect_.y+0.5*y_2;
+        deflect.y=deflect.y+0.5*y_2;
     elseif ModeRSS1OrMean2==1
         x_2=(eye(nspred)-oo.dr.ghx(nstatic+1:nstatic+nspred,:))\(oo.dr.ghs2(nstatic+1:nstatic+nspred,:));
         y_2=[oo.dr.ghx(1:nstatic,:)*x_2+oo.dr.ghs2(1:nstatic,:);x_2;oo.dr.ghx(nstatic+nspred+1:M.endo_nbr,:)*x_2+oo.dr.ghs2(nstatic+nspred+1:M.endo_nbr,:)];
-        deflect_.x_2=x_2;
-        deflect_.y=deflect_.y+0.5*y_2;
+        deflect.x_2=x_2;
+        deflect.y=deflect.y+0.5*y_2;
     end
 end
 if options.order==3 && ModeRSS1OrMean2 > 0
-    deflect_.y_x=deflect_.y_x+0.5*(oo.dr.ghxx*kron(x_2,eye(nspred))+2*(oo.dr.g_1(:,1:nspred)-oo.dr.ghx));
-    deflect_.y_u=deflect_.y_u+0.5*(oo.dr.ghxu*kron(x_2,eye(M.exo_nbr))+2*(oo.dr.g_1(:,1+nspred:end)-oo.dr.ghu));
+    deflect.y_x=deflect.y_x+0.5*(oo.dr.ghxx*kron(x_2,eye(nspred))+2*(oo.dr.g_1(:,1:nspred)-oo.dr.ghx));
+    deflect.y_u=deflect.y_u+0.5*(oo.dr.ghxu*kron(x_2,eye(M.exo_nbr))+2*(oo.dr.g_1(:,1+nspred:end)-oo.dr.ghu));
 end
-deflect_.y=deflect_.y(oo.dr.inv_order_var);
-deflect_.y_y=[zeros(M.endo_nbr,nstatic),deflect_.y_x,zeros(M.endo_nbr,nfwrd)];
-deflect_.y_y=deflect_.y_y(oo.dr.inv_order_var,oo.dr.inv_order_var);
-deflect_.y_e=deflect_.y_u(oo.dr.inv_order_var,:);
+deflect.y=deflect.y(oo.dr.inv_order_var);
+deflect.y_y=[zeros(M.endo_nbr,nstatic),deflect.y_x,zeros(M.endo_nbr,nfwrd)];
+deflect.y_y=deflect.y_y(oo.dr.inv_order_var,oo.dr.inv_order_var);
+deflect.y_e=deflect.y_u(oo.dr.inv_order_var,:);
